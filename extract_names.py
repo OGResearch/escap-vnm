e81f875178a29edacf94135d771ef5853c1a3e3c
Jaromír Beneš <jaromir.benes@gmail.com>
2023-10-30 12:17:58 +0100

    Extracted func names

--- extract_names.py
+++ extract_names.py
@@ -10,6 +10,8 @@ def _get_lhs_names(eqn: tuple[str, ...]) -> tuple[str, ...]:
     lhs = [ i.split("=")[0].strip() for i in eqn ]
     return tuple(_re.search(r"\w+\b(?!\()", i).group() for i in lhs)
 
+def _get_func_names(x: str) -> tuple[str, ...]:
+    return tuple(set(_re.findall(r"\b\w+\b(?=\()", x)))
 
 
 with open("escap-vnm.model", "r") as fid:
@@ -17,6 +19,10 @@ with open("escap-vnm.model", "r") as fid:
 
 with open("escap-vnm-externals.model", "r") as fid:
     x = fid.read()
+
+
+m = _re.sub(r"[`!]\w+", "", m, )
+x = _re.sub(r"[`!]\w+", "", x, )
 
 names = _re.findall(r"\b[a-z]\w*\b(?!\()", m)
 names = tuple(i for i in set(names) if not i.startswith("res_"))
@@ -29,6 +35,10 @@ rest = set(names).difference(vnm_names + wld_names + usa_names)
 
 if rest:
     raise ValueError("Unaccounted names")
+
+m_func_names = _get_func_names(m, )
+m_func_names = set(m_func_names).difference(("diff", "diff_log", "log", "exp"))
+
 
 m_eqn = _read_equations(m, )
 m_lhs_names = _get_lhs_names(m_eqn, )
@@ -47,5 +57,9 @@ with open("wld-variables.json", "w+") as fid:
 
 with open("usa-variables.json", "w+") as fid:
     _js.dump(usa_names, fid, indent=4)
+
+with open("func-names.json", "w+") as fid:
+    _js.dump(tuple(m_func_names), fid, indent=4)
+
 
 
